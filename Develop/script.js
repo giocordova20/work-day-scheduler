5ac4001878893cd5e22ca2de7f308c218bf519d5
giocode <giocordova@gmail.com>
2020-09-21 11:34:21 -0400

    completed code to add and pull tasks from local storage

--- Develop/script.js
+++ Develop/script.js
@@ -15,41 +15,51 @@ $(document).ready(function() {
     saveIcon.addClass=("fas fa-save");
 
     function loadtimeSlots() {
+        // Create the table to hold hour, task, and save button
         var scheduleTable = $("<table>");
         var scheduleTableBody = $("<tbody>");
         scheduleTable.append(scheduleTableBody);
         scheduleTable.addClass("table");
-        
         $(".container").append(scheduleTable);
-        // var hourLable = ["9AM","10AM","11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
         
         // Create the rows for each hour slot
         for (i = 0; i < 9; i++) {        
-            var timeSlotRow = $("<div class='row'>");
-            var timeSlotHour = $("<div class='col-1 text hour'>");
-            timeSlotHour.text(hourLable[i]);
+            var timeSlotRow = $("<div class='row'>");                   // Create div for the row
+            var timeSlotHour = $("<div class='col-1 text hour'>");      // Create div for the hour column
+            var timeSlotTask = $("<textarea>");                         // create columnn for textarea
+            timeSlotTask.addClass('col task '+hourLable[i]);            // Add the task class to textarea
+            var timeSlotSave = $("<div class='col-1 saveBtn text-center pt-4'>");   // Create div for the save button
+            var saveBtn =$("<i>").addClass("save-task fas fa-save save"+i);         // Create the save button with fontawesome
+            timeSlotHour.text(hourLable[i]);                            // Add hour lable to the hour column
+            var taskInStorage = localStorage.getItem(hourLable[i]);     // Task in local storage for the specified hour
+            
+            // Return data from local storage if it exists
+            if (taskInStorage){
+                timeSlotTask.text(taskInStorage);
+                console.log(taskInStorage);
+            }
 
-            var timeSlotTask = $("<textarea>");
-            timeSlotTask.addClass('col task '+hourLable[i]);
-            var timeSlotSave = $("<div class='col-1 saveBtn text-center pt-4'>");
-            var saveBtn =$("<i>").addClass("save-task fas fa-save save"+i);
-
+            // Create event listener for each save button 
             saveBtn.on("click", function(event){
-                // alert("HI");
-                console.log("");
-                console.log("save button has been clicked");
-                // console.log('Sibling Task: ', $(this).parent().siblings('textarea').val());
-                console.log('Sibling: ', $(this).parent().siblings('textarea').prev().text());
+                // Get the lable of the hour column to use as the key, and content of the textarea
+                // to save the task
                 var hour = $(this).parent().siblings('textarea').prev().text();
                 var task = $(this).parent().siblings('textarea').val();
+                
+                console.log("");
+                console.log("save button has been clicked");
+
+                // Add the task to local storage if there is an input
+                if (task != ""){
+                console.log('Sibling: ', $(this).parent().siblings('textarea').prev().text());
                 console.log("hour: ", hour, "task: ", task);
                 localStorage.setItem(hour, task);
+                }
                 console.log("");
             });
             
-            
-            timeSlotSave.append(saveBtn);
-            timeSlotRow.append(timeSlotHour,timeSlotTask,timeSlotSave);
+            timeSlotSave.append(saveBtn);                               // Append the save button to save column
+            timeSlotRow.append(timeSlotHour,timeSlotTask,timeSlotSave); // Append the completed row to the table
             
             scheduleTableBody.append(timeSlotRow);
         }
@@ -57,73 +67,7 @@ $(document).ready(function() {
         
     };
     
-    function getTask(hour){
-        if (localStorage.getItem("9AM")){
-            alert("There is data in local storage for 9AM")
-            console.log(localStorage.getItem("9AM"));
-            
-
-        }
-        if (localStorage.getItem("10AM")){
-            alert("There is data in local storage for 10AM")
-        }
-        if (localStorage.getItem("10AM")){
-            alert("There is data in local storage for 11AM")
-        }
-    }
     
-    getTask("9AM");
-
-    // $(".saveBtn1").on("click", function(event) {
-    //     event.preventDefault();
-        
-    //     // create user object from submission
-    //     var user = {
-    //       time: "9AM",
-    //       task: $("task").value()
-    //     };
-      
-    //     console.log(user);
-
-
-        
-    // var timeSlot = [
-    //   { 9: 'Task for 9am', 10: 'Task for 10:00am', 11: 'Task for 11:00am' },
-    // ]
-    // var timeSlot = {
-    //   '9am': 'Task for 9am',
-    //   10: 'Task for 10:00am',
-    //   11: 'Task for 11:00am',
-    // }
-    // var timeSlot = [
-    //   { '9am': { task1: 'Task for 9am', task2: 'Task for 9am' } },
-    //   { 10: 'Task for 10:00am' },
-    //   { 11: 'Task for 11:00am' },
-    // ];
-
-    // var customer = {
-    //   firstName: 'John',
-    //   lastName: 'Smith',
-    //   age: 25,
-    //   address: {
-    //     streetAddress: '21 2nd Street',
-    //     city: 'New York',
-    //     state: 'NY',
-    //     postalCode: '10021',
-    //   },
-    //   phoneNumber: [
-    //     {
-    //       type: 'home',
-    //       number: '212 555-1234',
-    //     },
-    //     {
-    //       type: 'fax',
-    //       number: '646 555-4567',
-    //     },
-    //   ],
-    // ;}
-
-
 
     loadtimeSlots();
 });
